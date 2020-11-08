a351b899c0df087430da965ccec12896dd4ad229
Gustavo Allen <gustavoallen92@gmail.com>
2020-11-08 03:43:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -128,3 +128,13 @@ def play(board)
     puts "Cat's Game!"
 end
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
